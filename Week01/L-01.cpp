98bb2db452e990743e7ee9896f2b7407cc151c1e
Kumari Ranjana Yadav <yranjana757@gmail.com>
2023-09-18 22:35:41 +0530

    done week 1 class-02

--- Week01/L-01.cpp
+++ Week01/L-01.cpp
@@ -3,29 +3,19 @@ using namespace std;
 
 int main()
 {
-    //! Multiply 2 number by taking inputs .
+    // cout << "Hello world :) " << endl;
+    // cout << "enter a number " << endl;
+    // int a;
+    // cin >> a;
+    // cout << "You entered " << a << endl;
+    int number = 12 ;
+    char ch = 'A' ;
+    float pi = 3.14;
+    cout << number << endl ;
+    cout << ch << endl;
+    cout << pi << endl;
 
-    // int num1, num2;
-    // cout << "Enter the value of num1 and num2 " << endl;
-    // cin >> num1 >> num2;
-    // cout << "The output is " << num1 * num2 << endl;
-
-    //! Find the perimeter of a trianglr .
-    // int side1, side2, side3;
-    // cout << "Enter the side1 , side2 and side3 " << endl;
-    // cin >> side1 >> side2 >> side3;
-    // int perimter = side1 + side2 + side3;
-    // cout << "The perimeter of triangle of Triangle is " << perimter << endl;
-
-    //! Find the Simple Interest
-    // int p, r, t;
-    // cout << "Enter the value of P , R , T " << endl;
-    // cin >> p >> r >> t;
-    // cout << "The Simple Interrest is " << (p * r * t) / 100 << endl;
-
-    // ! Print counitng from N to 1
-
-    // ! Find factorial of a number
-
-    //! Check Prime or not
+    // ? TYPE CASCTING ..
+    double pi2 = 3.45 ;
+    cout << (int)pi + 3 <<endl;
 }
